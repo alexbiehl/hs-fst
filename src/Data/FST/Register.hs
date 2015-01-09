4fc3a2adc70f9c03833a55f21e16e37ce9a6b0d6
Alex Biehl <abiehl@novomind.com>
2015-01-09 16:59:30 +0100

    Simple binary compilation

--- src/Data/FST/Register.hs
+++ src/Data/FST/Register.hs
@@ -3,15 +3,23 @@ module Data.FST.Register(
   , finalArc
   , empty
   , replaceOrRegister
+  , finalize
   ) where
 
-import Data.FST.Types
-import Foreign.Storable
-import Data.Word (Word32)
-import Data.HashMap.Strict (HashMap)
+import           Data.FST.Types
+
+import           Blaze.ByteString.Builder (Builder)
+import qualified Blaze.ByteString.Builder as Blaze
+import qualified Blaze.ByteString.Builder.Internal.Write as Blaze
+import qualified Blaze.ByteString.Builder.Word as Blaze
+import           Data.ByteString (ByteString)
+import           Data.Monoid
+import           Data.Word (Word32)
+import           Data.HashMap.Strict (HashMap)
 import qualified Data.Foldable as Foldable
 import qualified Data.HashMap.Strict as HashMap
 import qualified Data.List as List
+import           Foreign.Storable
 
 data Entry = E !StateRef !Word32
             deriving (Eq, Show)
@@ -19,12 +27,14 @@ data Entry = E !StateRef !Word32
 data Register a = Register {
     regStates   :: !(HashMap [Arc] Entry)
   , regNextRef  :: !StateRef
-  } deriving (Eq, Show)
+  , regBuilder  :: !Builder
+  }
 
 empty :: Register a
 empty = Register {
     regStates  = HashMap.fromList []
   , regNextRef = 1
+  , regBuilder = mempty
   }
 
 finalArc :: Arc
@@ -34,23 +44,45 @@ finalArc = Arc {
   , arcTarget   = 0
   }
 
+
+finalize :: Register a -> ByteString
+finalize = Blaze.toByteString . regBuilder
+
 replaceOrRegister ::
   UncompiledState a
   -> Register a
   -> (Arc, Register a)
-replaceOrRegister node register =
+replaceOrRegister (UncompiledState label arcs output) register =
   case HashMap.lookup arcs (regStates register) of
    Just (E stateRef n) -> (Arc label n stateRef, register)
    Nothing             -> (Arc label numWords (regNextRef register), register')
   where
-    UncompiledState label arcs output = node
-
     register' = register {
         regStates  = HashMap.insert arcs
                      (E (regNextRef register) numWords) (regStates register)
       , regNextRef = (regNextRef register) + 1
+      , regBuilder = compileArcs arcs `mappend` regBuilder register
       }
 
     numWords = Foldable.foldl' (+) 0
                $ fmap arcNumWords
                $ arcs
+
+compileArcs :: [Arc] -> Builder
+compileArcs arcs = Blaze.fromWrite final
+  where
+    (arcCount, _, write) = go arcs
+    final                =
+      Blaze.writeWord32be arcCount `mappend` write
+
+    go :: [Arc] -> (Word32, Word32, Blaze.Write)
+    go []     = (0, 0, mempty)
+    go (a:ax) = (n', weight', built')
+      where
+        (n, weight, built) = go ax
+        n'              = n + 1
+        weight'         = arcNumWords a + weight
+        built'          = built `mappend` arc
+        arc             = Blaze.writeWord8 (arcLabel a)
+          `mappend` Blaze.writeWord32be weight'
+          `mappend` Blaze.writeWord64be (arcTarget a)
